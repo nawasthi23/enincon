9b16de613fe51af9b1cbd5b3f102543695427d8a
Nishant Awasthi <Nishant Awasthi>
2016-05-21 18:20:14 +0530

    first shot in the sky

---
+++ subscription/src/main/java/in/enincon/engine/ServletInitializer.java
@@ -0,0 +1,13 @@
+package in.enincon.engine;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SubscriptionApplication.class);
+	}
+
+}
